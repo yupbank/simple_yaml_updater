b750a408e8c7a329ec198b573d60c1a82f1007cb
Peng Yu <peng.yu@shopify.com>
2016-10-04 11:02:17 -0400

    add file

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Simple inplace YAML editor
+-----
+a very simple inplace yaml updater and seperator.
+
+basically the yaml.load and yaml.dump isnt keeping the originally format. So it is the tool to help you with some really simple operation.
+
+which is going to keep the origin format.
---
+++ simple_yaml_updater.py
@@ -0,0 +1,140 @@
+from string import strip
+import datetime
+
+def left_blank_count(line):
+    count = 0
+    for i in line:
+        if i != '':
+            return count
+        else:
+            count += 1
+    return count
+
+get_back = lambda x: ' '.join(x)
+
+
+def line_key(line, ident_blank_count):
+    return get_back(map(lambda x: x.replace(':', ''), line[ident_blank_count: ident_blank_count+1]))
+
+
+def pop_stack(stack, length):
+    while length>0:
+        stack.pop()
+        length -= 1
+
+def seperate_yaml(to_move, file_location):
+    origin_yaml, detached_yaml = [], []
+    previous_old = True
+    stack = []
+    prev_blank_count, blanks_per_ident = 0, 2
+    for line in open(file_location):
+        line = line.rstrip().split(' ')
+        ident_blank_count = left_blank_count(line)
+        if ident_blank_count == len(line):
+            prev_blank_count = 0
+            if previous_old:
+                origin_yaml.append(get_back(line))
+            else:
+                detached_yaml.append(get_back(line))
+                previous_old = True
+            continue
+        if ident_blank_count == 0:
+            key = get_back(line)
+            stack = [key]
+        if prev_blank_count > ident_blank_count:
+            pop_count = (prev_blank_count-ident_blank_count)/blanks_per_ident+1
+            pop_stack(stack, pop_count)
+            stack.append(line_key(line, ident_blank_count))
+
+        if prev_blank_count < ident_blank_count:
+            if prev_blank_count == 0:
+                blanks_per_ident = ident_blank_count-prev_blank_count
+
+            stack.append(line_key(line, ident_blank_count))
+
+        if prev_blank_count == ident_blank_count:
+            stack.pop()
+            stack.append(line_key(line, ident_blank_count))
+
+        #change
+        top_key = stack[0]
+        if top_key in to_move:
+            previous_old = False
+            detached_yaml.append(get_back(line))
+        else:
+            origin_yaml.append(get_back(line))
+
+        prev_blank_count = ident_blank_count
+    return origin_yaml, detached_yaml
+
+def update_yaml(to_update={}, file_location='starscream/conf/schedule.yml'):
+    new_yaml = []
+    stack = []
+    prev_blank_count, blanks_per_ident = 0, 2
+    for line in open(file_location):
+        line = line.rstrip().split(' ')
+        ident_blank_count = left_blank_count(line)
+        if ident_blank_count == len(line):
+            prev_blank_count = 0
+            new_yaml.append(get_back(line))
+            continue
+        if ident_blank_count == 0:
+            key = get_back(line)
+            stack = [key]
+        if prev_blank_count > ident_blank_count:
+            pop_count = (prev_blank_count-ident_blank_count)/blanks_per_ident+1
+            pop_stack(stack, pop_count)
+            stack.append(line_key(line, ident_blank_count))
+
+        if prev_blank_count < ident_blank_count:
+            if prev_blank_count == 0:
+                blanks_per_ident = ident_blank_count-prev_blank_count
+
+            stack.append(line_key(line, ident_blank_count))
+
+        if prev_blank_count == ident_blank_count:
+            stack.pop()
+            stack.append(line_key(line, ident_blank_count))
+        #change
+        current_key = '.'.join(stack)
+        if current_key in to_update:
+            line[-1] = to_update[current_key]            
+
+        new_yaml.append(get_back(line))
+
+        prev_blank_count = ident_blank_count
+    return new_yaml
+
+def time_arrangement(begin, length):
+    new_begin_times = []
+    for _ in xrange(length):
+        lag = _*10*60
+        new_begin_times.append((begin+datetime.timedelta(seconds=lag)).strftime('%H,%M,AM,UTC'))
+    return new_begin_times
+
+
+def move_task():
+    to_move = {'key_one', 'key_two'}
+    origin_file_location = 'test.yml'
+    updated_origin_yaml, detached_yaml = seperate_yaml(to_update, origin_file_location)
+
+    with open('detached_origin.yml', 'w') as f:
+        f.write('\n'.join(updated_origin_yaml))
+
+    with open('detached.yml', 'w') as f:
+        f.write('\n'.join(detached_yaml))
+
+def update_task():
+    to_update = {'key_one.second_key': 'new_value', 'key_two.second_key.third_key': 'new_value'}
+    origin_file_location = 'test.yml'
+    updated_origin_yaml = update_yaml(to_update, origin_file_location)
+
+    with open('updated_origin.yml', 'w') as f:
+        f.write('\n'.join(updated_origin_yaml))
+
+def main():
+    move_task()
+    update_task()
+
+if __name__ == "__main__":
+    main()
